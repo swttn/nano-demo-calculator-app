1c814537ac16d627e86c5ede6b530fa4d5da7872
swttn <sweetogood@gmail.com>
2023-09-05 15:44:20 +0530

    solution

--- server.py
+++ server.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
-def add():
-    return ''
+def add(first, second):
+    return f'result-of-the-summation:{first + second}'
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    return ''
+def subtract(first, second):
+    return f'result-of-the-subtraction:{first - second}'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
